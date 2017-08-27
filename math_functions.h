9a6ed146a68568d90a75a9d18e654183406bee7d
michael-delegge <michael.w.delegge@gmail.com>
2017-08-26 23:18:43 -0400

    Header for conversion functions

---
+++ math_functions.h
@@ -0,0 +1,7 @@
+double convert32(uint32_t num);
+double convert64(uint64_t num);
+uint32_t swap24(uint8_t *val);
+uint32_t swap32(uint32_t val);
+uint64_t swap64(uint64_t val);
+double binary32(uint32_t i);
+double binary64(uint64_t i);
